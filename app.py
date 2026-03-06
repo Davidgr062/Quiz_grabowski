341954c7cc1889f1662b5ac219506dd5468317b2
elias reichelt <reichelteli13@.com>
2026-03-06 10:36:20 +0100

    y<dsr

--- app.py
+++ app.py
@@ -3,18 +3,18 @@ from flask import Flask, render_template, request, redirect, url_for
  
 app = Flask(__name__)
  
-# Datenbank-Verbindung
+
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
  
-# Startseite
+
 @app.route('/')
 def start():
     return render_template('start.html')
 
-# Lehrer-Seite / Board anzeigen
+
 @app.route('/lehrer')
 def index():
     conn = get_db_connection()
@@ -22,14 +22,14 @@ def index():
     conn.close()
     return render_template('index.html', text_quiz=text_quiz)
  
-# Quiz-Seite: Fragen anzeigen und beantworten
+
 @app.route('/quiz', methods=['GET', 'POST'])
 def quiz():
     conn = get_db_connection()
     text_quiz = conn.execute('SELECT * FROM text_quiz').fetchall()
    
     if request.method == 'POST':
-        # Antworten überprüfen
+        
         erstelltes_Quiz  = []
         for quiz in text_quiz:
             frage1_id = quiz['Id']
@@ -58,7 +58,7 @@ def quiz():
     conn.close()
     return render_template('quiz.html', text_quiz=text_quiz)
  
-# Neue Nachricht hinzufügen
+
 @app.route('/add', methods=['POST'])
 def add_post():
     quiz_titel = request.form.get('quiz_titel')
@@ -76,7 +76,7 @@ def add_post():
  
     return redirect(url_for('index'))
  
-# Nachricht löschen
+
 @app.route('/delete/<int:id>', methods=['POST'])
 def delete_post(id):
     conn = get_db_connection()
